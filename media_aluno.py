7a100a6ab7864b54dfdd34658d231f5d5f3a057a
Geovanerdev <110861856+DpooL033@users.noreply.github.com>
2026-02-01 10:52:23 -0300

    Fix print statement for media output and approval message

--- media_aluno.py
+++ media_aluno.py
@@ -11,13 +11,13 @@ nota2 = float(input("Digite a segunda nota: "))
 # A media vai receber a nota1 mais a nota2 e dividir em 2 e guardar o resultado.
 media = (nota1 + nota2) / 2
 
-print(media, "Média")
+print("Média", media)
 
 # o if vai verificar se a media é maior ou igual a 7.
 if media >= 7:
 
 # O print vai imprimir na tela se (Aprovado) se a media for maior ou igual a 7 
-  print(nome, "Aprovado!")
+  print(nome, "Foi provado!")
 
 # O else - se não vai imprimir (Reprovado!)
 else:
