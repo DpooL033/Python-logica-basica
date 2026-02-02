b9e99487d01b7f221bf91be1b26214c4cd6bb63d
Geovanedev <110861856+DpooL033@users.noreply.github.com>
2026-02-02 19:57:49 -0300

    produtos.py
    Implement simple product registration in Python with total cost calculation and product count above R$100.

---
+++ lógica-python-produtos.py
@@ -0,0 +1,36 @@
+#cadastro simples de produtos em Python
+Este projeto é um exercício de lógica de programação em Python.
+
+## Funcionalidades
+- Cadastro de produtos (nome e preço)
+- Soma do valor total dos produtos
+- Contagem de produtos com preço acima de R$100
+
+## Conceitos utilizados 
+- Entrada e saída de dados
+- Estrutura de repetição(for)
+- Estrutura condicional (if)
+- Contador e acumulador
+
+## Objetivo
+Praticar lógica de programação e registrar a evolução nos estudos em Python.
+
+-------------------------------------------------------------
+
+quantidade = int(input("Quantos produtos deseja cadastrar? "))
+
+total = 0 
+produtos_mais_100 = 0
+
+for i range(quantidade):
+    nome = input("Nome do produto: ")
+    preco = float(input("preço do produto: "))
+
+    total += preco
+
+    if preco > 100:
+      produtos_mais_100 += 1
+
+print("\mResumo da compra")
+print("Total gasto: R$", total)
+print("Produtos acima de R$100:", produtos_mais_100)
